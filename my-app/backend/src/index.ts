6e415d0304ba3e84369263ffcda5d0831c85e5d0
Jonathan Magdanz <jm@skillme.de>
2024-10-06 12:45:27 +0200

    structure and initialization

---
+++ my-app/backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import authRoutes from "./routes/auth";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mysql from "mysql2";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use("/api/auth", authRoutes);
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {});
